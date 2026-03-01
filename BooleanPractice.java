a0c8a849d0e0f0aefbfc09c0b55099c7ade96637
gshiladakis <66285313+gshiladakis@users.noreply.github.com>
2026-02-28 19:28:27 -0500

    Create BooleanPractice.java

---
+++ BooleanPractice.java
@@ -0,0 +1,22 @@
+public class BooleanPractice {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       boolean boolean0 = true;
+       boolean boolean1 = false;
+       System.out.println("boolean0 is " + boolean0);
+       System.out.println("boolean1 is " + boolean1);
+       
+       boolean equals = (2 == 3);
+       boolean notEquals = (2 != 3);
+       boolean greaterThan = (2 > 3);
+       boolean lessThan = (2 < 3);
+       System.out.println("(2 == 3) is " +equals);
+       System.out.println("(2 != 3) is " +notEquals);
+       System.out.println("(2 > 3) is " +greaterThan);
+       System.out.println("(2 < 3) is " +lessThan);
+    }
+    
+}
